3cd6897ec195a013413e7a14b7e28abcc6c75815
Nikolay <areyfclm@gmail.com>
2023-06-19 20:07:41 +0300

    redux persist was added to project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Provider } from 'react-redux';
-import {store} from './redux/index'
-import { BrowserRouter } from 'react-router-dom';
-import { RoutesMap } from './routes';
-import './firebase';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/index";
+import { BrowserRouter } from "react-router-dom";
+import { RoutesMap } from "./routes";
+import "./firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <RoutesMap />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <RoutesMap />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
